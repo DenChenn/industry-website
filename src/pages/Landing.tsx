8243ec6dab216d06090a8a5731342f8de8df0cee
SwarzChen <aaaa102234@gmail.com>
2021-02-26 23:24:01 +0800

    [main] add image gallery

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import Background from '../components/Background'
+import { Col, Row } from "react-flexbox-grid";
 import Content from '../components/Content'
 import NavBar from '../components/NavBar'
+import ImageGallery from '../components/ImageGallery'
 import '../styles/Landing.css'
 
 const Landing = () => {
     return (
         <div> 
-            <div className="landing">
-                <NavBar/>
-                <Content/>
+            <div className="landing" style={{backgroundImage: "url(/images/background.jpg)"}}>
+                <div className="cover">
+                    <NavBar/>
+                    <Content/>
+                </div>
+            </div>
+            <div>
+                <ImageGallery />
             </div>
         </div>
     )
