b86e7847c8094a62e1bc96386526df6809e4ac15
SwarzChen <aaaa102234@gmail.com>
2021-07-04 21:17:53 +0800

    :sparkles: add navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+import HamburgerMenu from '../hamburgerMenu'
+
+const NavBar = () => {
+  return (
+    <div className="navbar-container">
+      <HamburgerMenu></HamburgerMenu>
+    </div>
+  )
+}
+
+export default NavBar
