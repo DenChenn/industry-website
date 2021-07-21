bd5a14fd799db32fdaeb301984fcf68c0ca618c6
SwarzChen <aaaa102234@gmail.com>
2021-07-21 22:37:51 +0800

    :fire: redefine routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Landing from './pages/landing'
+import Dinitro from './pages/dinitro'
+
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
         <Switch>
-          <Route path="/" component={Landing}></Route>
-          
+          <Route path="/about" component={Landing}></Route>
+          <Route path="/dinitro" component={Dinitro}></Route>
         </Switch>
       </div>
     </BrowserRouter>
