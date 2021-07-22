be08f853756723d3157859dbcaba2853bad5e2b4
SwarzChen <aaaa102234@gmail.com>
2021-07-22 22:44:50 +0800

    :rocket: new routing path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Landing from './pages/landing'
 import Dinitro from './pages/dinitro'
+import Generator from './pages/generator'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Landing}></Route>
           <Route path="/dinitro" component={Dinitro}></Route>
+          <Route path="/generator" component={Generator}></Route>
         </Switch>
       </div>
     </BrowserRouter>
