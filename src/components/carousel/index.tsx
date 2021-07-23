c3042aba03d2a3964cb7eb42782395c4d93b68ed
SwarzChen <aaaa102234@gmail.com>
2021-07-23 21:34:03 +0800

    :rocket: fix deploy

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -7,15 +7,15 @@ const ImportCarousel = () => {
     <div className="c-container">
       <Carousel infiniteLoop useKeyboardArrows autoPlay>
         <div>
-          <img height="auto" src="images/s1.jpg" />
+          <img height="auto" src="images/s1.jpg" alt="#" />
           <p className="legend">甘肅慶陽</p>
         </div>
         <div>
-          <img height="auto" src="images/s2.jpg" />
+          <img height="auto" src="images/s2.jpg" alt="#" />
           <p className="legend">江蘇鎮江宏順熱電廠</p>
         </div>
         <div>
-          <img height="auto" src="images/s4.jpg" />
+          <img height="auto" src="images/s4.jpg" alt="#" />
           <p className="legend">中國巨石集團</p>
         </div>
       </Carousel>
