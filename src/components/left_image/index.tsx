2ea57ca57f5f3c11ca24a1758ee8124b40555bbb
SwarzChen <aaaa102234@gmail.com>
2021-07-23 14:05:07 +0800

    :tada: add image section animation

--- src/components/left_image/index.tsx
+++ src/components/left_image/index.tsx
@@ -1,19 +1,85 @@
 import './index.css'
+import { useState, useRef, useLayoutEffect } from 'react'
+import { motion } from 'framer-motion'
 
 const LeftImage = (props: {
   imageUrl: string
   title: string
   content: string
 }) => {
+  const [isOpen, setOpen] = useState(false)
+  const refer = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    const topPos = refer.current?.getBoundingClientRect().top
+
+    const onScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight
+
+      if (refer != null) {
+        if (topPos && topPos < scrollPosition) {
+          setOpen(true)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  const variant = {
+    open: {
+      opacity: 1,
+      transform: 'translateY(0%)',
+    },
+    closed: {
+      opacity: 0,
+      transform: 'translateY(50%)',
+    },
+  }
+
+  const transition = {
+    duration: 0.5,
+  }
+
+  const delayTransition = {
+    duration: 0.5,
+    delay: 0.3,
+  }
+
+  const imageTransition = {
+    duration: 0.5,
+    delay: 0.6,
+  }
+
   return (
-    <div className="left-image-container">
+    <div className="left-image-container" ref={refer}>
       <div className="left">
-        <div className="title">{props.title}</div>
-        <div className="content">{props.content}</div>
+        <motion.div
+          className="title"
+          animate={isOpen ? 'open' : 'closed'}
+          variants={variant}
+          transition={transition}
+        >
+          {props.title}
+        </motion.div>
+        <motion.div
+          className="content"
+          animate={isOpen ? 'open' : 'closed'}
+          variants={variant}
+          transition={delayTransition}
+        >
+          {props.content}
+        </motion.div>
       </div>
-      <div className="right">
+      <motion.div
+        className="right"
+        animate={isOpen ? 'open' : 'closed'}
+        variants={variant}
+        transition={imageTransition}
+      >
         <img width="100%" height="100%" src={props.imageUrl} alt="#" />
-      </div>
+      </motion.div>
     </div>
   )
 }
